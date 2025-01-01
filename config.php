31df6f97a1585e7b72eb48f33238cbba90e9e3ed
Andre <andremoreiraguimaraes@gmail.com>
2025-01-01 02:34:45 -0300

    mais atualizações no php

--- config.php
+++ config.php
@@ -3,5 +3,13 @@
     $dbHost = 'Localhost';
     $dbUsername = 'root';
     $dbPassword = 'teste123';
+    $dbName = 'banco-de-dados-formulario';
 
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);// para se conectar ao banco de dados
+
+    if($conexao->connect_error){
+        echo "Erro";
+    }else {
+        echo "conexão efetuada com sucesso.";
+    }
 ?>
